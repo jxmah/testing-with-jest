345b3abd131934a317d400209f8765352f74d578
jxmah <jxmah.23@gmail.com>
2025-05-07 12:24:49 +0200

    Lade till pop-test för stack-funktionalitet (testet gick inte igenom)

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,12 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop returns the correct element', () => {
+    let expectedElement = "apple";
+    stack.push(expectedElement);
+
+    let poppedElement = "banana";
+
+    expect(poppedElement).toBe(expectedElement);
+});
